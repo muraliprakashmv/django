c01da39e90a09eaf6968ff937abc58ff108b4033
mvmurali006 <email@example.com>
2018-11-27 12:02:18 +0530

    MySample Django red to blue

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'js^f-o2$ick^j1)p2nsxs%!9+d2lq%_wm##9l63!ldgquo$oh6'
+SECRET_KEY = '###############################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
